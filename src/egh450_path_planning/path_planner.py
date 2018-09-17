545ecaf7ab8506356bfd36302858918fd64de5cc
tfordinoz <tw.ford@connect.qut.edu.au>
2018-09-17 12:18:38 +1000

    week 9 test update

--- src/egh450_path_planning/path_planner.py
+++ src/egh450_path_planning/path_planner.py
@@ -26,57 +26,88 @@ class PathPlanner():
 	def shutdown(self):
 		# Unregister anything that needs it here
 		pass
+	def e_to_q(self,roll,pitch,yaw):
+		cy = cos(yaw * 0.5);
+		sy = sin(yaw * 0.5);
+		cr = cos(roll * 0.5);
+		sr = sin(roll * 0.5);
+		cp = cos(pitch * 0.5);
+		sp = sin(pitch * 0.5);
+
+		w = cy * cr * cp + sy * sr * sp;
+		x = cy * sr * cp - sy * cr * sp;
+		y = cy * cr * sp + sy * sr * cp;
+		z = sy * cr * cp - cy * sr * sp;
+
+		return (w,x,y,z)
 
 	def request_path(self,msgin):
 		rospy.loginfo("[NAV] Requesting path from Breadcrumb")
 		msg_out = Path()
 		msg_out.header = msgin.header
 	
-		for i in range(len(msgin.waypoints)):
-			#Request a path from breadcrumb
-			req = RequestPathRequest()
-		
-			req.start = msgin.waypoints[i].position
-			req.end = msgin.waypoints[i+1].position
-
-			res = self.srvc_bc(req);
-		
-			if len(res.path.poses) > 0:
-	
-				#Insert the start pose
+		for i in range(len(msgin.waypoints) - 1):
+			if msgin.waypoints[i].position.x == msgin.waypoints[i+1].position.x and msgin.waypoints[i].position.y == msgin.waypoints[i+1].position.y:
 				ps = PoseStamped()
-				ps.header = res.path.header
-				ps.pose.position = req.start
+				ps.header = msgin.header
+				ps.header.seq = i
 				ps.pose.orientation.w = 1.0
-				ps.pose.orientation.x = 0.0
-				ps.pose.orientation.y = 0.0
-				ps.pose.orientation.z = 0.0
+				ps.pose.position.x = msgin.waypoints[i].position.x
+				ps.pose.position.y = msgin.waypoints[i].position.y
+				ps.pose.position.z = msgin.waypoints[i].position.z
 				msg_out.poses.append(ps)
-			
-
-				# Insert the path recieved from breadcrumb
-				for sp in res.path.poses[0::5]:
-					p = PoseStamped()
-					p.header = res.path.header
-					p.pose.position = sp.position
-					p.pose.orientation.w = 1.0
-					p.pose.orientation.x = 0.0
-					p.pose.orientation.y = 0.0
-					p.pose.orientation.z = 0.0
-					msg_out.poses.append(p)
-
-				#Insert the end pose
-				pe = PoseStamped()
-				pe.header = res.path.header
-				pe.pose.position = req.end
-				pe.pose.orientation.w = 1.0
-				pe.pose.orientation.x = 0.0
-				pe.pose.orientation.y = 0.0
-				pe.pose.orientation.z = 0.0
-				msg_out.poses.append(pe)
-				
 			else:
-				rospy.logerr("[NAV] No path received, abandoning planning")
+				
+				#Request a path from breadcrumb
+				req = RequestPathRequest()
+			
+				req.start = msgin.waypoints[i].position
+				req.end = msgin.waypoints[i+1].position
+
+				res = self.srvc_bc(req);
+			
+				if len(res.path.poses) > 0:
+		
+					#Insert the start pose
+					ps = PoseStamped()
+					ps.header = res.path.header
+					ps.pose.position = req.start
+					ps.pose.orientation.w = 1.0
+					ps.pose.orientation.x = 0.0
+					ps.pose.orientation.y = 0.0
+					ps.pose.orientation.z = 0.0
+					msg_out.poses.append(ps)
+						
+					
+			
+					# Insert the path recieved from breadcrumb
+					for sp in res.path.poses:
+						p = PoseStamped()	
+						p.header = res.path.header
+						p.pose.position = sp.position
+						p.pose.orientation.w = 1.0
+						p.pose.orientation.x = 0.0
+						p.pose.orientation.y = 0.0
+						p.pose.orientation.z = 0.0
+						(w,x,y,z) = self.e_to_q(0.0,0.0,msgin.waypoints[i].yaw)
+						p.pose.orientation.w = w
+						p.pose.orientation.x = x
+						p.pose.orientation.y = y
+						p.pose.orientation.z = z
+						msg_out.poses.append(p)
+
+					#Insert the end pose
+					pe = PoseStamped()
+					pe.header = res.path.header
+					pe.pose.position = req.end
+					pe.pose.orientation.w = 1.0
+					pe.pose.orientation.x = 0.0
+					pe.pose.orientation.y = 0.0
+					pe.pose.orientation.z = 0.0
+					msg_out.poses.append(pe)
+						
+				else:
+					rospy.logerr("[NAV] No path received, abandoning planning")
 		self.pub_path.publish(msg_out)
 
 
